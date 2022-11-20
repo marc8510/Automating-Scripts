4723b0668f6070f6f111bf10cc7f374dabe4549b
Marc Edelmann <marc.edelmann@gmx.net>
2022-11-20 13:54:51 +0100

    basic input for connection to db

--- sql-ops/database/connection_details.py
+++ sql-ops/database/connection_details.py
@@ -1,1 +1,39 @@
+import pyodbc
+#OnPremise SSMS Server
+onPremiseConnection = pyodbc.connect(
+          "DRIVER={ODBC driver 17 for SQL Server};"
+          "Server=xxx;"
+          "Database=xxx;"
+          "Trusted Connection=yes;"
+)
+onPremisecursor =onPremiseConnection.cursor()
+onPremisecursor.execute("#SQL SCRIPT#")
 
+result = onPremisecursor.fetchall()
+
+for onPremiseRow in result:
+    print(onPremiseRow)
+    print("\n")
+
+onPremisecursor.close()
+onPremiseConnection.close()
+
+#Azure Server
+
+azureConnection = pyodbc.connect(
+               "DRIVER={ODBC driver 17 for SQL Server};"
+               "Server=xxx;"
+               "Database=xxx;"
+               "Uid=xxx;"
+               "Pwd=xxx;"
+               "Trusted Connection=no;"
+               "Connection Timeout=30;"
+)
+
+azureCursor = azureConnection.cursor()
+azureCursor.execute("#SQL SCRIPT#")
+azureRow = azureCursor.fetchone()
+
+while azureCursor:
+    print(str(azureRow[0]) + " " + str(azureRow[1]))
+    azureRow = azureCursor.fetchone()
